281a272e0ada7b51de91eddb267d39fe91204158
Anuj Shankar <anujshankar95@gmail.com>
2017-02-24 09:10:46 +0530

    Done with functionalities, test remaining.

--- youtube-video-search/src/components/App.js
+++ youtube-video-search/src/components/App.js
@@ -1,31 +1,30 @@
 import React, { Component } from 'react'
 import YTSearch from 'youtube-api-search'
+import '../index.css'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
       video: [],
-      playingVideo: '' 
+      currVideo: {}
     }
   }
-
   update(videoData) {
-    console.log(videoData)
     this.setState({ video: videoData })
   }
 
-  playVideo(){
-    
+  playVideo(videoData) {
+    this.setState({ currVideo: videoData })
   }
 
   render() {
-    console.log(this.state)
     return (
       <div className="App">
         <h2>Youtube Search</h2>
         <Search update={this.update.bind(this)} />
-        <VideoList content={this.state.video} />
+        <VideoList content={this.state.video} playVideo={this.playVideo.bind(this)} />
+        <VideoPreview play={this.state.currVideo} />
       </div>
     )
   }
@@ -51,14 +50,13 @@ class Search extends Component {
 
 class VideoList extends Component {
   render() {
-    console.log('1', this.props.content)
-    let videoList = this.props.content.map(function (video, index) {
+    let videoList = this.props.content.map((video, index) => {
       return <li key={index}>
-        <VideoThumbnail imageUrl={video.snippet.thumbnails.default.url} videoId={video.id.videoId} title={video.snippet.title}/>
+        <VideoThumbnail imageUrl={video.snippet.thumbnails.default.url} videoId={video.id.videoId} title={video.snippet.title} playVideo={this.props.playVideo} />
       </li>
     })
     return (
-      <ul>
+      <ul className='video-list'>
         {videoList}
       </ul>
     )
@@ -66,28 +64,34 @@ class VideoList extends Component {
 }
 
 class VideoThumbnail extends Component {
-  update(){
-
+  player() {
+    this.props.playVideo({ videoId: this.props.videoId, title: this.props.title })
   }
   render() {
+    console.log('Inside VideoThumbnail', this.props.playVideo)
     return (
-    <div>
-      <img src={this.props.imageUrl} alt='img didnt load' onClick={this.update.bind(this)} />
-      <div>{this.props.title}</div>
-    </div>
+      <div>
+        <img src={this.props.imageUrl} alt='Sorry!' onClick={this.player.bind(this)} />
+        <div>{this.props.title}</div>
+      </div>
     )
   }
 }
 
 class VideoPreview extends Component {
   render() {
-    return (
-    <div>
-      <img src={this.props.imageUrl} alt='img didnt load' />
-      <div>{this.props.title}</div>
-    </div>
-    )
+    if (!this.props.play.videoId) {
+      return (<div></div>)
+    }
+    else {
+      const url = `https://www.youtube.com/embed/${this.props.play.videoId}`
+      const title = this.props.play.title
+      return (
+        <div>
+          <iframe src={url} height={"350px"} width={"500px"} />
+          <h3>Title : {title}</h3>
+        </div>
+      )
+    }
   }
 }
-
-
